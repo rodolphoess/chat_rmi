26780be9f9bf9ab2cce09340547bc5525887e0a0
rodolpho.erick <rodolpho.erick@A303-22L.imd.local>
2019-03-07 16:13:49 -0300

    Servidor

---
+++ chat_server/src/chat_server/ChatServer.java
@@ -0,0 +1,15 @@
+package chat_server;
+
+public class ChatServer {
+	
+	public static void main(String[] argv) {
+		
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Erro no chat: " + e);
+		}
+	}
+
+}
