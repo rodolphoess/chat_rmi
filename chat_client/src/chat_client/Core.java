78200519eff508c72936be64ee407216090c365b
Luis Eduardo Rique <eduardo_ri_que@hotmail.com>
2019-03-07 15:54:55 -0300

    rascunho cliente 2

--- chat_client/src/chat_client/Core.java
+++ chat_client/src/chat_client/Core.java
@@ -1,57 +1,5 @@
 package chat_client;
 
-import java.rmi.*;
-import javax.swing.*;
-import java.util.Scanner;
-import java.lang.Thread.*;
-import java.util.ArrayList;
-import java.rmi.RemoteException;
-
-public class Cliente {
-
-	public static void main( String args[] ) {
-		try {
-			final ServidorChat chat = (ServidorChat) Naming.lookup( “rmi://192.168.102.11:1098/ServidorChat” );
+public class Core {
 	
-			String nome;
-			String msg = “”;
-			Scanner scanner = new Scanner(System.in);
-
-			System.out.println(“Digite seu nome:”);
-
-			nome = scanner.nextLine();
-			
-			Thread thread = new Thread(new Runnable() {
-			int cont = chat.lerMensagem().size();
-			
-	@Override
-	public void run() {
-		try {
-			while(true){
-				if (chat.lerMensagem().size() > cont){
-					System.out.println(chat.lerMensagem().get(chat.lerMensagem().size()-1));
-					cont++;
-					}
-			}
-		} catch (RemoteException e) {
-			e.printStackTrace();
-			}
-	}
-			}); thread.start();
-
-		while(msg != “exit”){
-
-			System.out.println(nome+”: “);
-
-			msg = scanner.nextLine();
-
-			chat.enviarMensagem(nome+”: “+msg);
-			// System.out.println(chat.lerMensagem().get(cont));
-		}
-
-		}
-		catch( Exception e ) {
-			e.printStackTrace();
-		}
-	}
 }
